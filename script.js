1a9d488a50d57799744b0a2ad22840828b852f87
HankFontaine <henrymcgill@gmail.com>
2022-06-09 11:03:15 -0400

    fixed more bugs, including decimal point issues

--- script.js
+++ script.js
@@ -3,40 +3,38 @@
 ///////////////////////////////////////////////////////////////
 
 let memoryValue = 0;
-let firstInputValue = '';
-let secondInputValue = '';
-let operator = '';
+let firstInputValue = "";
+let secondInputValue = "";
+let operator = "";
 
-let displayStrip = document.createElement('div');
+let displayStrip = document.createElement("div");
 displayStrip.innerHTML = 0;
-document.getElementById('display').appendChild(displayStrip);
+document.getElementById("display").appendChild(displayStrip);
 
-const allClear = document.getElementById('allClear');
-const clearBtn = document.getElementById('clear');
-const memory = document.getElementById('memory');
-const changePositive = document.getElementById('changePositive');
-const seven = document.getElementById('seven');
-const eight = document.getElementById('eight');
-const nine = document.getElementById('nine');
-const divide = document.getElementById('divide');
-const four = document.getElementById('four');
-const five = document.getElementById('five');
-const six = document.getElementById('six');
-const multiply = document.getElementById('multiply');
-const one = document.getElementById('one');
-const two = document.getElementById('two');
-const three = document.getElementById('three');
-const subtract = document.getElementById('subtract');
-const zero = document.getElementById('zero');
-const decimal = document.getElementById('decimal');
-const exponent = document.getElementById('exponent');
-const add = document.getElementById('add');
+const allClear = document.getElementById("allClear");
+const clearBtn = document.getElementById("clear");
+const memory = document.getElementById("memory");
+const changePositive = document.getElementById("changePositive");
+const seven = document.getElementById("seven");
+const eight = document.getElementById("eight");
+const nine = document.getElementById("nine");
+const divide = document.getElementById("divide");
+const four = document.getElementById("four");
+const five = document.getElementById("five");
+const six = document.getElementById("six");
+const multiply = document.getElementById("multiply");
+const one = document.getElementById("one");
+const two = document.getElementById("two");
+const three = document.getElementById("three");
+const subtract = document.getElementById("subtract");
+const zero = document.getElementById("zero");
+const decimal = document.getElementById("decimal");
+const exponent = document.getElementById("exponent");
+const add = document.getElementById("add");
 
-allClear.addEventListener('click', function () {
-  console.log('all clear clicked');
+allClear.addEventListener("click", function () {
   memoryValue = 0;
   clear();
-  // this is all repeated logic that you can move out to a 'clear' function and re-use for 'clear' and reuse for checkForScreenText
 });
 
 ///////////////////////////////////////////////////////////////
@@ -47,19 +45,19 @@ function operate(firstInputValue, operator, secondInputValue) {
   const parsedNum1 = Number(firstInputValue);
   const parsedNum2 = Number(secondInputValue);
 
-  if (operator === 'add') {
-    calculatedResult = parsedNum1 + parsedNum2; // You want this to be consistent, make everything a number in this function
+  if (operator === "add") {
+    calculatedResult = parsedNum1 + parsedNum2;
   }
-  if (operator === 'subtract') {
+  if (operator === "subtract") {
     calculatedResult = parsedNum1 - parsedNum2;
   }
-  if (operator === 'multiply') {
+  if (operator === "multiply") {
     calculatedResult = parsedNum1 * parsedNum2;
   }
-  if (operator === 'exponent') {
+  if (operator === "exponent") {
     calculatedResult = parsedNum1 ** parsedNum2;
   }
-  if (operator === 'divide') {
+  if (operator === "divide") {
     if (parsedNum2 === 0) {
       calculatedResult = null;
     } else calculatedResult = parsedNum1 / parsedNum2;
@@ -72,86 +70,68 @@ function operate(firstInputValue, operator, secondInputValue) {
 ///////////////////////////////////////////////////////////////
 
 function getButtonValue(number) {
-  if (displayStrip.innerHTML === '0') {
+  if (displayStrip.innerHTML === "0") {
     displayStrip.innerHTML = number;
   } else displayStrip.innerHTML += number;
 }
 
-one.addEventListener('click', function () {
+one.addEventListener("click", function () {
   checkForScreenText();
   getButtonValue(1);
 });
-two.addEventListener('click', function () {
+two.addEventListener("click", function () {
   checkForScreenText();
   getButtonValue(2);
 });
-three.addEventListener('click', function () {
+three.addEventListener("click", function () {
   checkForScreenText();
   getButtonValue(3);
 });
-four.addEventListener('click', function () {
+four.addEventListener("click", function () {
   checkForScreenText();
   getButtonValue(4);
 });
-five.addEventListener('click', function () {
+five.addEventListener("click", function () {
   checkForScreenText();
   getButtonValue(5);
 });
-six.addEventListener('click', function () {
+six.addEventListener("click", function () {
   checkForScreenText();
   getButtonValue(6);
 });
-seven.addEventListener('click', function () {
+seven.addEventListener("click", function () {
   checkForScreenText();
   getButtonValue(7);
 });
-eight.addEventListener('click', function () {
+eight.addEventListener("click", function () {
   checkForScreenText();
   getButtonValue(8);
 });
-nine.addEventListener('click', function () {
+nine.addEventListener("click", function () {
   checkForScreenText();
   getButtonValue(9);
 });
-zero.addEventListener('click', function () {
+zero.addEventListener("click", function () {
   checkForScreenText();
   getButtonValue(0);
 });
-decimal.addEventListener('click', function () {
-  checkForScreenText();
-  if (displayStrip.innerHTML.includes('.')) {
-    displayStrip.innerHTML = 'DECLINED';
-  } else if (displayStrip.innerHTML === 0) {
-    displayStrip.innerHTML = '0.';
-  } else displayStrip.innerHTML += '.';
-
-  // if operator is defined allow two periods
-  // if operator is defined allow two periods
-  // if operator is defined allow two periods
-  // if operator is defined allow two periods
-  // if operator is defined allow two periods
-  // if operator is defined allow two periods
-  // if operator is defined allow two periods
-  // if operator is defined allow two periods
-  // if operator is defined allow two periods
-  // if operator is defined allow two periods
-  // if operator is defined allow two periods
-  // if operator is defined allow two periods
+decimal.addEventListener("click", function () {
+  addDecimal();
 });
 
 ///////////////////////////////////////////////////////////////
 /////////////// GET OPERATOR INPUT ////////////////////////////
 ///////////////////////////////////////////////////////////////
 
-clearBtn.addEventListener('click', function () {
+clearBtn.addEventListener("click", function () {
   clear();
 });
 
-changePositive.addEventListener('click', function () {
+changePositive.addEventListener("click", function () {
   changePositveNegative();
 });
 
-memory.addEventListener('click', function () {
+memory.addEventListener("click", function () {
   if (displayStrip.innerHTML == 0) {
     displayStrip.innerHTML = Number(memoryValue);
   } else if (memoryValue != 0) {
@@ -159,33 +139,33 @@ memory.addEventListener('click', function () {
   } else memoryValue = Number(displayStrip.innerHTML);
 });
 
-divide.addEventListener('click', function () {
+divide.addEventListener("click", function () {
   checkForOperator();
-  operator = 'divide';
+  operator = "divide";
   firstInputValue = displayStrip.innerHTML;
   displayStrip.innerHTML = `${displayStrip.innerHTML} / `;
 });
-multiply.addEventListener('click', function () {
+multiply.addEventListener("click", function () {
   checkForOperator();
-  operator = 'multiply';
+  operator = "multiply";
   firstInputValue = displayStrip.innerHTML;
   displayStrip.innerHTML = `${displayStrip.innerHTML} X `;
 });
-subtract.addEventListener('click', function () {
+subtract.addEventListener("click", function () {
   checkForOperator();
-  operator = 'subtract';
+  operator = "subtract";
   firstInputValue = displayStrip.innerHTML;
   displayStrip.innerHTML = `${displayStrip.innerHTML} - `;
 });
-add.addEventListener('click', function () {
+add.addEventListener("click", function () {
   checkForOperator();
-  operator = 'add';
+  operator = "add";
   firstInputValue = displayStrip.innerHTML;
   displayStrip.innerHTML = `${displayStrip.innerHTML} + `;
 });
-exponent.addEventListener('click', function () {
+exponent.addEventListener("click", function () {
   checkForOperator();
-  operator = 'exponent';
+  operator = "exponent";
   firstInputValue = displayStrip.innerHTML;
   displayStrip.innerHTML = `${displayStrip.innerHTML} ^ `;
 });
@@ -194,22 +174,22 @@ exponent.addEventListener('click', function () {
 /////////////// USE "ENTER" BUTTON TO GET RESULTS//////////////
 ///////////////////////////////////////////////////////////////
 
-enterButton.addEventListener('click', function () {
-  secondInputValue = displayStrip.innerHTML.split(' ').pop();
-  if (firstInputValue === '' || operator === '' || secondInputValue === '') {
-    displayStrip.innerHTML = 'DECLINED';
+enterButton.addEventListener("click", function () {
+  secondInputValue = displayStrip.innerHTML.split(" ").pop();
+  if (firstInputValue === "" || operator === "" || secondInputValue === "") {
+    displayStrip.innerHTML = "DECLINED";
     return;
   } else {
     const res = operate(firstInputValue, operator, secondInputValue);
     if (res === null) {
-      displayStrip.innerHTML = 'DECLINED';
+      displayStrip.innerHTML = "DECLINED";
     } else {
       displayStrip.innerHTML = roundDecimal(res);
     }
   }
-  firstInputValue = '';
-  secondInputValue = '';
-  operator = '';
+  firstInputValue = "";
+  secondInputValue = "";
+  operator = "";
 });
 
 ///////////////////////////////////////////////////////////////
@@ -217,11 +197,11 @@ enterButton.addEventListener('click', function () {
 ///////////////////////////////////////////////////////////////
 
 function checkForOperator() {
-  if (operator !== '') {
-    secondInputValue = displayStrip.innerHTML.split(' ').pop();
+  if (operator !== "") {
+    secondInputValue = displayStrip.innerHTML.split(" ").pop();
     let newValue = operate(firstInputValue, operator, secondInputValue);
     if (newValue === null) {
-      displayStrip.innerHTML = 'DECLINED';
+      displayStrip.innerHTML = "DECLINED";
     } else {
       displayStrip.innerHTML = newValue;
     }
@@ -246,9 +226,9 @@ function roundDecimal(inputNumber) {
 
 function checkForScreenText() {
   if (
-    displayStrip.innerHTML === 'DECLINED' ||
-    displayStrip.innerHTML === 'NaN' ||
-    displayStrip.innerHTML === 'Infinity'
+    displayStrip.innerHTML === "DECLINED" ||
+    displayStrip.innerHTML === "NaN" ||
+    displayStrip.innerHTML === "Infinity"
   ) {
     clear();
   }
@@ -259,16 +239,16 @@ function checkForScreenText() {
 ///////////////////////////////////////////////////////////////
 
 function operatorCheck() {
-  if (operator != '') {
-    displayStrip.innerHTML = 'DECLINED';
+  if (operator != "") {
+    displayStrip.innerHTML = "DECLINED";
   }
 }
 
 function clear() {
-  displayStrip.innerHTML = '0';
-  firstInputValue = '';
-  secondInputValue = '';
-  operator = '';
+  displayStrip.innerHTML = "0";
+  firstInputValue = "";
+  secondInputValue = "";
+  operator = "";
 }
 
 ///////////////////////////////////////////////////////////////
@@ -277,7 +257,29 @@ function clear() {
 
 function changePositveNegative() {
   if (operator) {
-    let modifiedValue = displayStrip.innerHTML.split(' ').pop() * -1;
-    displayStrip.innerHTML = displayStrip.innerHTML.slice(0, -1) + modifiedValue;
+    let modifiedValue = displayStrip.innerHTML.split(" ").pop() * -1;
+    displayStrip.innerHTML =
+      displayStrip.innerHTML.slice(0, -1) + modifiedValue;
   } else displayStrip.innerHTML = displayStrip.innerHTML * -1;
+}
+
+///////////////////////////////////////////////////////////////
+///////////////// ADD DECIMAL POINTS //////////////////////////
+///////////////////////////////////////////////////////////////
+
+function addDecimal() {
+  let tempSecondValue;
+  checkForScreenText();
+  if (operator) {
+    tempSecondValue = displayStrip.innerHTML.split(" ").pop();
+    if (tempSecondValue == "") {
+      displayStrip.innerHTML = displayStrip.innerHTML + "0.";
+      return;
+    }
+    if (!tempSecondValue.includes(".")) {
+      displayStrip.innerHTML = displayStrip.innerHTML + ".";
+    }
+  } else if (displayStrip.innerHTML === 0) {
+    displayStrip.innerHTML = "0.";
+  } else displayStrip.innerHTML += ".";
 }
